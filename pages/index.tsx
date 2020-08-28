59b8adcb44d0736a8c02546d910f40a310796bfd
Samuel Reis <sa.reis1999@gmail.com>
2020-08-28 15:46:24 -0300

    inserido caminho absoluto no markdown

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import { GetServerSideProps } from "next";
 import fs from "fs";
 import { promisify } from "util";
 import Home from '../containers/Home'
-
+import path from 'path'
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   let file;
   try {
-    file = await promisify(fs.readFile)("./texts/HomePage.md", "utf8");
+    file = await promisify(fs.readFile)(path.resolve('texts', 'HomePage.md'), "utf8");
   } catch (error) {}
 
   return {
