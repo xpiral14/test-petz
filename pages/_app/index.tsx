a1a73b127e2f9f6c9b6a8bef642d4446154551f2
Samuel Reis <sa.reis1999@gmail.com>
2020-08-28 15:24:27 -0300

    Removido código morto

--- pages/_app/index.tsx
+++ pages/_app/index.tsx
@@ -1,7 +1,6 @@
 import Header from "../../components/Header";
-import GlobalStyle from "../../theme/GlobalStyle";
+import GlobalStyle from "../../styles/GlobalStyle";
 import { Container, Content } from "./style";
-import { Head } from "next/document";
 
 function MyApp({ Component, pageProps }) {
   return (
