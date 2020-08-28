27993adaee12dae331aa6bdfb40b38dabfec586d
Samuel Reis <sa.reis1999@gmail.com>
2020-08-28 14:01:34 -0300

    Adiconado tema de cores

---
+++ theme/GlobalStyle.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @import url('"https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap');
+*{
+    padding: none;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+    font-family: 'Poppins', sans-serif;
+}
+body{
+}
+`;
+
+export default GlobalStyle;
---
+++ theme/index.ts
@@ -0,0 +1,19 @@
+const theme = {
+  colors: {
+    primary: "#00A0E4",
+    secondary: "#FFD400",
+    danger: "#FF0505",
+    success: "#408140",
+    warning: "#FFD400",
+    info: "#00A0E4",
+    light: "#fff",
+  },
+
+  text: {
+    color1: "#555",
+    color2: "#262626",
+    color3: "#6E6E6E",
+  },
+};
+
+export default theme;
