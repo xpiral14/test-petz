b40e9791253a616b6ce89d2697c31460f7877dc6
Samuel Reis <sa.reis1999@gmail.com>
2020-08-28 15:24:16 -0300

    Migrado arquivo de estilo global para a pasta de arquivos

---
+++ styles/GlobalStyle.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+    padding: none;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+    font-family: Poppins, 'Arial', sans-serif;
+}
+body{
+}
+`;
+
+export default GlobalStyle;
